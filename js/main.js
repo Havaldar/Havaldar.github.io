12b27679e6a5aebf394f4cd06deb4d598f40d9ff
Abhinav <abhinavhavaldar@hotmail.com>
2016-11-28 01:48:28 -0500

    hehe

--- js/main.js
+++ js/main.js
@@ -153,6 +153,9 @@ app.controller('MainCtrl', function($scope, $sce) {
 		'emacs': function() {
 			return '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0" frameborder="0"></iframe><br>';
 		},
+		'nano': function() {
+			return '<img src="https://media.giphy.com/media/w6MNHOoLEzBVm/giphy.gif" style="filter: grayscale(100%);">'
+		},
 		'whoami': function() { 
 			return "Good question. You think you are " + $scope.guest + ", but who really knows.<br>"; 
 		},
